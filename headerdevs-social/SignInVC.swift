6817f113e4872f518cb8fc90d5269d117ef6a53e
Header-Dev <headerdev@gmail.com>
2017-07-17 14:19:27 +0700

    create project and config.

---
+++ headerdevs-social/SignInVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  headerdevs-social
+//
+//  Created by kritawit bunket on 7/17/2560 BE.
+//  Copyright © 2560 Headerdevs. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
