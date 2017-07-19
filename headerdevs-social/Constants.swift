bba78015abc425be249ddfe0b46406379f7a1095
Header-Dev <headerdev@gmail.com>
2017-07-19 13:36:57 +0700

    Sign In ui custom classes complete

---
+++ headerdevs-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  headerdevs-social
+//
+//  Created by kritawit bunket on 7/19/2560 BE.
+//  Copyright © 2560 Headerdevs. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY : CGFloat = 120.0 / 255.0
---
+++ headerdevs-social/RoundBtn.swift
@@ -0,0 +1,29 @@
+//
+//  RoundBtn.swift
+//  headerdevs-social
+//
+//  Created by kritawit bunket on 7/19/2560 BE.
+//  Copyright © 2560 Headerdevs. All rights reserved.
+//
+
+import UIKit
+
+class RoundBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: SHADOW_GRAY).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+}
