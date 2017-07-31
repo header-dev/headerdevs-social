d0d918c2e1879b5db2a7c692361b4ff6608ea9fc
Header-Dev <headerdev@gmail.com>
2017-07-31 13:25:18 +0700

    Completed email auth

--- headerdevs-social/SignInVC.swift
+++ headerdevs-social/SignInVC.swift
@@ -13,6 +13,9 @@ import FirebaseAuth
 import Firebase
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +49,27 @@ class SignInVC: UIViewController {
             }
         }
     }
+    
+    @IBAction func signInTapped(_ sender: Any){
+     
+        if let email = emailField.text, let pwd = pwdField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("JESS : Email user authenticated with Firebase")
+                }else{
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("JESS : Unable to authenticate with Firebase using email")
+                        }else{
+                            print("JESS : Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+        
+    }
+    
     
     
     override func didReceiveMemoryWarning() {
