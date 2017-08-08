499dc03bc1c6013dfdaa9f654416003227290eaa
Header-Dev <headerdev@gmail.com>
2017-08-08 12:51:37 +0700

    Firebase get completed

--- headerdevs-social/FeedVC.swift
+++ headerdevs-social/FeedVC.swift
@@ -15,6 +15,8 @@ class FeedVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,8 +24,19 @@ class FeedVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
         tableView.dataSource = self
 
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value!)
+            if let snapshots = snapshot.children.allObjects as? [DataSnapshot]{
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String,Any>{
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
+        
         // Do any additional setup after loading the view.
     }
     
@@ -33,10 +46,14 @@ class FeedVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("JESS: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
